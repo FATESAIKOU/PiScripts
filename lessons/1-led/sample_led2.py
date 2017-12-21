cb152ba3289114ad67be19af29ee0411f905caba
FATESAIKOU <qzsecftbhhhh@gmail.com>
2017-12-21 11:40:43 +0800

    Add pwm_sample to 1-led

--- lessons/1-led/sample_led2.py
+++ lessons/1-led/sample_led2.py
@@ -19,41 +19,48 @@ def end_handler(signal, frame):
     print("end of shining")
     shining = False
 
+
 def initEnv():
     GPIO.setmode(GPIO.BOARD)
 
+
 def initPin(pins):
+    pwms = []
     for ledPin in pins:
         GPIO.setup(ledPin, GPIO.OUT)
+        t_pwm = GPIO.PWM(ledPin, 500)
+        t_pwm.start(100)
+        
+        pwms.append(t_pwm)
 
-def setupPin(pins):
-    for ledPin in pins:
-        GPIO.output(ledPin, True)
+    return pwms
+        
 
-def setoffPin(pins):
-    for ledPin in pins:
-        GPIO.output(ledPin, False)
+def setPin(pwms, duties):
+    for i in range(len(pwms)):
+        pwms[i].ChangeDutyCycle(duties[i])
+
 
 def main():
     PINS = [int(s) for s in sys.argv[1:]]
 
     print("Init Env")
     initEnv()
-    initPin(PINS)
+    pwms = initPin(PINS)
 
     signal.signal(signal.SIGINT, end_handler)
 
     while shining:
-        now_pins = random.sample(PINS, random.randint(1, 3))
-        print("Use Pin:", now_pins)
+        duties = [random.randint(0, 100) for _ in range(len(pwms))]
+        print("Use Duties:", duties)
     
         print("Setup Pins")
-        setupPin(now_pins)
+        setPin(pwms, duties)
         time.sleep(random.uniform(0.1, 0.7))
+
     
-        print("setoff Pins")
-        setoffPin(now_pins)
-    
+    print("Setoff Pins")
+    setPin(pwms, [0] * len(pwms))
     GPIO.cleanup()
 
 
